fc8eff9ffa8c51907bd0a38134aaa6a20ffeb41c
malindaw <you@example.com>
2024-04-18 12:45:50 +0530

    intcom

---
+++ PaymentHub/src/test/java/com/swaglabs/config/TestRunner.java
@@ -0,0 +1,16 @@
+package com.swaglabs.config;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "",
+		glue = ""		
+		)
+
+public class TestRunner {
+
+}
